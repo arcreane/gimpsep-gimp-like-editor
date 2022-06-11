3f7c54ca016fbfb8e4931cf0e83aba49068e4c93
siv-isep <sika61518@eleve.isep.fr>
2022-06-11 17:49:44 +0200

    Recall all available actions

--- src/models/headers/UserAction.h
+++ src/models/headers/UserAction.h
@@ -10,13 +10,21 @@ class UserAction{
                 {1, "DILATATION"},      //size
                 {2, "EROSION"},         //size
                 {3, "RESIZING"},        //newHeight, newWidth
-                {4, "LIGHTEN"},         //lightFactor
-                {5, "DARKEN"},          //lightFactor
-                {6, "STITCHING"},       //No parameter
-                {7, "EDGE DETECTION"},  //No parameter
-                {8, "HISTORY"},         //No parameter
-                {9, "UNDO"},            //No parameter
-                {-1, "QUIT"},
+                {4, "BRIGHTNESS"},      //lightFactor
+                {5, "STITCHING"},       //No parameter
+                {6, "EDGE DETECTION"},  //No parameter
+
+                {7, "CONVERT TO GREY"},  //TP2
+                {8, "CROP"},             //TP2
+                {9, "ROTATE"},          //TP2
+
+                {10, "CONTRAST"},        //TP3
+
+                {11, "BLUR"},            //TP5
+
+                {12, "HISTORY"},         //No parameter
+                {13, "UNDO"},            //No parameter
+                {-1, "QUIT"},            //No parameter
         };
 
         static string getActionName(int key);
