751b17f63e8e338ca75ade16d4cbe15d6027ba50
siv-isep <sika61518@eleve.isep.fr>
2022-06-11 16:21:39 +0200

    Implements Observer Pattern between AppController and GimpApplication

---
+++ src/observer/Observer.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "../models/Image.h"
+
+#pragma once
+
+class Subject;
+class Observer{
+public:
+    virtual ~Observer() = default;
+
+    virtual void beforeChangeUpdate(Subject& changedSubject) = 0;
+
+    virtual void afterChangeUpdate(Subject& changedSubject, Image image, string specialAction) = 0;
+};
